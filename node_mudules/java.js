a57ee84a2d89a170554a4c82875150aed42edde8
khalid <khmohomudhersi@gmail.com>
2024-11-19 12:43:47 +0200

    of every

--- node_mudules/java.js
+++ node_mudules/java.js
@@ -1,10 +1,3 @@
-// const mobileMenu = document.getElementById('mobile-menu');
-// const navLinks = document.querySelector('.nav-links');
-
-// mobileMenu.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-// });
-
 const header = document.getElementById('header');
 const menuToggle = document.getElementById('menu-toggle');
 const navbar = document.querySelector('.navbar');
